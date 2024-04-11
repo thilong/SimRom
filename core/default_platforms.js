6bbc2f6bd0dfd1e22aef5f6f40ff750986e7ba2b
thilong <thilong@126.com>
2024-04-11 17:20:22 +0800

    add some config.

---
+++ core/default_platforms.js
@@ -0,0 +1,12 @@
+module.exports.defaultPlatforms = [
+    {
+        name: "fc",
+        fullName: "Nintendo Famicom",
+        ext: ".nes",
+    },
+    {
+        name: "sfc",
+        fullName: "Super Nintendo Entertainment System",
+        ext: ".sfc",
+    }
+]
